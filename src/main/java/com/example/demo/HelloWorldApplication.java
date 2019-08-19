82cd3a33c924683958495f40c12af2686257e8be
NoraYu <yxy_1990@hotmail.com>
2019-08-19 11:59:45 -0400

    first commit

---
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import javax.xml.ws.RequestWrapper;
+
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String arg[]){
+        SpringApplication.run(HelloWorldApplication.class, arg);
+
+
+
+	}
+
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.xml.ws.RequestWrapper;
+
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homepage () {
+        return "index";
+
+
+
+    }
+
+}
